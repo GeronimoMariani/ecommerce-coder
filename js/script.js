1559f3734aa67e58df8404ea8b588fdfcb680063
Geronimo Mariani <geronimomariani5@gmail.com>
2023-06-30 21:21:07 -0300

    se agrego json

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ let apellidoLocal = localStorage.getItem("apellido");
 let edadLocal = localStorage.getItem("edad");  */
 
 
-class Bebidas {
+/* class Bebidas {
     constructor(id, categoria, nombre, precio, stock, image, alt) {
         this.id = id;
         this.categoria = categoria;
@@ -59,85 +59,99 @@ productos.push(new Bebidas("cerveza-03", "cervezas", "Corona 710cc", 759, 26, ".
 productos.push(new Bebidas("whisky-01", "whiskies", "Jack Daniels Honey", 18743, 10, "./images/whisky-jack-daniels-honey-750-ml--.jpg", "Jack Daniels Honey"));
 productos.push(new Bebidas("whisky-02", "whiskies", "Jack Daniels Old", 18743, 15, "./images/whisky-jack-daniels-old-no-7-750-cc--.jpg", "Jack Daniels Old"));
 productos.push(new Bebidas("whisky-03", "whiskies", "Johnnie Walker Black Label", 15155, 13, "./images/whisky-johnnie-walker-black-label-750ml.jpg", "Johnnie Walker Black Label"));
+productos.push(new Bebidas("whisky-04", "whiskies", "Johnnie Walker Blue Label", 102000, 3, "./images/whisky-johnnie-walker-blue-label-750ml.jpg", "Johnnie Walker Blue Label"));
 productos.push(new Bebidas("aperitivo-01", "aperitivos", "Fernet Branca", 2417, 20, "./images/fernet-branca---750-cc--.jpg", "Fernet Branca"));
 productos.push(new Bebidas("vino-01", "vinos", "Vino Varietal terra Malbec Roble", 1450, 18, "./images/vino-varietal-terra-malbec-roble-750-cc--.jpg", "Vino Varietal terra Malbec Roble"));
 productos.push(new Bebidas("vino-02", "vinos", "Vino Varietal Dada Chocolate", 1040, 23, "./images/vino-varietal-dada-n§8-chocolate-750-cc--.jpg", "Vino Varietal Dada Chocolate"));
-productos.push(new Bebidas("vino-03", "vinos", "Mosquita Muerta Blend Tintas", 6400, 8, "./images/vinos-premium-mosquita-muerta-blend-tintas-750-cc--.jpg", "Mosquita Muerta Blend Tintas"));
+productos.push(new Bebidas("vino-03", "vinos", "Mosquita Muerta Blend Tintas", 6400, 8, "./images/vinos-premium-mosquita-muerta-blend-tintas-750-cc--.jpg", "Mosquita Muerta Blend Tintas")); */
 
-console.log(productos);
 
 let gridContainer = document.getElementById("grid-container");
 let btnCarrito = document.querySelectorAll(".btnCarrito");
-console.log(gridContainer);
 
-function agregarProductos (productosElegidos) {
-    gridContainer.innerHTML = "";
-    productosElegidos.forEach(producto => {
-        const gridItem = document.createElement("div");
-        gridItem.className = "grid-item";
-        gridItem.innerHTML = `<img id="producto-imagen" src="${producto.image}" alt="${producto.alt}">
-                                <span><hr></span>
-                                <h3 id="producto-precio">$${producto.precio}</h3>
-                                <h4 id="producto-nombre">${producto.nombre}</h4>
-                                <h5 id="producto-stock">Stock: ${producto.stock}</h5>
-                                <button class= "btnCarrito" id="${producto.id}">Agregar al carrito</button>`
-        gridContainer.appendChild(gridItem);
-    })
-    actualizarBotonCarrito()
-}
 
-agregarProductos(productos)
 
-let botonNav = document.querySelectorAll(".navButton");
-let tituloPrincipal = document.querySelector("#titulo-principal");
-const contadorCarrito = document.querySelector("#contadorCarrito")
 
-botonNav.forEach(nav => {
-    nav.addEventListener("click", (e) => {
-        if (e.currentTarget.id != "todos") {
-            const seleccionTitulo = productos.find(producto => producto.categoria === e.currentTarget.id);
-            tituloPrincipal.innerText = seleccionTitulo.categoria.toUpperCase();
-            const seleccionNav = productos.filter(producto => producto.categoria === e.currentTarget.id);
-            agregarProductos(seleccionNav);
-        }else {
-            tituloPrincipal.innerText = "TODOS LOS PRODUCTOS";
-            agregarProductos(productos);
+const pedirProductos = async () => {
+    const resp = await
+    fetch("/productos.json")
+    productos = await resp.json()
+    productos.forEach(() => {
+        function agregarProductos (productosElegidos) {
+            gridContainer.innerHTML = "";
+            productosElegidos.forEach(producto => {
+                const gridItem = document.createElement("div");
+                gridItem.className = "grid-item";
+                gridItem.innerHTML = `<img id="producto-imagen" src="${producto.image}" alt="${producto.alt}">
+                                        <span><hr></span>
+                                        <h3 id="producto-precio">$${producto.precio}</h3>
+                                        <h4 id="producto-nombre">${producto.nombre}</h4>
+                                        <h5 id="producto-stock">Stock: ${producto.stock}</h5>
+                                        <button class= "btnCarrito" id="${producto.id}">Agregar al carrito</button>`
+                gridContainer.appendChild(gridItem);
+            })
+            actualizarBotonCarrito()
+        }
+        
+        agregarProductos(productos)
+        
+        let botonNav = document.querySelectorAll(".navButton");
+        let tituloPrincipal = document.querySelector("#titulo-principal");
+        const contadorCarrito = document.querySelector("#contadorCarrito")
+        
+        botonNav.forEach(nav => {
+            nav.addEventListener("click", (e) => {
+                if (e.currentTarget.id != "todos") {
+                    const seleccionTitulo = productos.find(producto => producto.categoria === e.currentTarget.id);
+                    tituloPrincipal.innerText = seleccionTitulo.categoria.toUpperCase();
+                    const seleccionNav = productos.filter(producto => producto.categoria === e.currentTarget.id);
+                    agregarProductos(seleccionNav);
+                }else {
+                    tituloPrincipal.innerText = "TODOS LOS PRODUCTOS";
+                    agregarProductos(productos);
+                }
+            })
+        })
+        
+        function actualizarBotonCarrito() {
+            btnCarrito = document.querySelectorAll(".btnCarrito");
+            btnCarrito.forEach((selectCarrito) => {
+            selectCarrito.addEventListener("click", agregarCarrito);
+            })
+        }
+        
+        const carrito = []
+        
+        function agregarCarrito(e){
+            const idBoton = e.currentTarget.id;
+            const productoAgregado = productos.find(producto => producto.id === idBoton);
+            if(carrito.some(producto => producto.id === idBoton)) {
+                const indexCarrito = carrito.findIndex(producto => producto.id === idBoton);
+                carrito[indexCarrito].cantidad++
+            }else {
+                productoAgregado.cantidad = 1;
+                carrito.push(productoAgregado);
+            }
+            Swal.fire({
+                icon: 'success',
+                title: 'Genial!',
+                text: 'Su producto a sido agregado correctamente!',
+                showConfirmButton: false,
+                timer: 1000,
+            })
+            actualizarContadorCarrito()
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            console.log(carrito)
+        }
+        
+        function actualizarContadorCarrito() {
+            let contador = carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+            contadorCarrito.innerHTML = contador;
         }
     })
-})
-
-function actualizarBotonCarrito() {
-    btnCarrito = document.querySelectorAll(".btnCarrito");
 }
 
-btnCarrito.forEach((selectCarrito) => {
-    selectCarrito.addEventListener("click", agregarCarrito);
-})
+pedirProductos()
 
-const carrito = []
 
-function agregarCarrito(e){
-    const idBoton = e.currentTarget.id;
-    const productoAgregado = productos.find(producto => producto.id === idBoton);
-    if(carrito.some(producto => producto.id === idBoton)) {
-        const indexCarrito = carrito.findIndex(producto => producto.id === idBoton);
-        carrito[indexCarrito].cantidad++
-    }else {
-        productoAgregado.cantidad = 1;
-        carrito.push(productoAgregado);
-    }
-    Swal.fire({
-        icon: 'success',
-        title: 'Genial!',
-        text: 'Su producto a sido agregado correctamente!',
-        showConfirmButton: false,
-        timer: 1000,
-    })
-    actualizarContadorCarrito()
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-}
 
-function actualizarContadorCarrito() {
-    let contador = carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
-    contadorCarrito.innerHTML = contador;
-}
